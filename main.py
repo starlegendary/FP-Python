7774bf289672f4332abcaed27832cfc85875551f
spy02 <billshum08@gmail.com>
2021-03-08 05:59:58 +0000

    fib logn recursion

--- main.py
+++ main.py
@@ -12,3 +12,22 @@ def sth(a):
         return a==b
     return inner
 print(sth(10)(10))
+def fib3(n):
+  
+    if(n == 0 or n == 1): return [[1,1],[1,0]]
+    fff = fib3(n//2)
+    def mul(A,B):
+        BT = [[B[j][i] for j in range(len(B))] for i in range(len(B[0]))]
+        return list(map(lambda a: 
+                    list(
+                      map(lambda b:
+                      sum(map(lambda x,y: x*y,a,b)),BT)
+                    )
+                    ,A))
+    if n%2 == 0:return mul(fff,fff) 
+    else: return mul(mul(fff,fff),[[1,1],[1,0]])
+
+aa = []
+dd ='aa'
+aa+=list(dd)
+print(aa)
